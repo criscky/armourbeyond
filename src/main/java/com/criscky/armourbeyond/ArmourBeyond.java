3ea30d90ac2b529d08a18b75406f54a4a205ba04
criscky <cristianyoshida@ymail.com>
2021-10-27 17:19:18 -0300

    update

--- src/main/java/com/criscky/armourbeyond/ArmourBeyond.java
+++ src/main/java/com/criscky/armourbeyond/ArmourBeyond.java
@@ -18,7 +18,7 @@ import org.apache.logging.log4j.Logger;
 
 import java.util.stream.Collectors;
 
-// The value here should match an entry in the META-INF/mods.toml file test
+// The value here should match an entry in the META-INF/mods.toml file
 @Mod("armourbeyond")
 public class ArmourBeyond
 {
