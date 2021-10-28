182dd62f4a2c19536470ffa3aa2bc487a6d83f02
criscky <cristianyoshida@ymail.com>
2021-10-28 09:58:51 -0300

    more

---
+++ src/main/java/com/criscky/armourbeyond/data/DataGenerators.java
@@ -0,0 +1,21 @@
+package com.criscky.armourbeyond.data;
+
+import com.criscky.armourbeyond.ArmourBeyond;
+import net.minecraft.data.DataGenerator;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.GatherDataEvent;
+
+@Mod.EventBusSubscriber(modid = ArmourBeyond.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class DataGenerators {
+    private DataGenerators() {}
+
+    @SubscribeEvent
+    public static void gatherData(GatherDataEvent event) {
+        DataGenerator gen = event.getGenerator();
+        ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
+
+    }
+
+}
