f7c4788277329281f5cdb94f51d868f147d0fb65
criscky <cristianyoshida@ymail.com>
2021-11-03 14:57:26 -0300

    stuff

--- src/main/java/com/criscky/armourbeyond/data/client/blockStateProvider.java
+++ src/main/java/com/criscky/armourbeyond/data/client/blockStateProvider.java
@@ -2,9 +2,18 @@ package com.criscky.armourbeyond.data.client;
 
 import com.criscky.armourbeyond.ArmourBeyond;
 import com.criscky.armourbeyond.setup.ModBlocks;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.data.DataGenerator;
+import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.util.Direction;
+import net.minecraftforge.client.model.generators.BlockModelBuilder;
 import net.minecraftforge.client.model.generators.BlockStateProvider;
+import net.minecraftforge.client.model.generators.ConfiguredModel;
+import net.minecraftforge.client.model.generators.ModelFile;
 import net.minecraftforge.common.data.ExistingFileHelper;
+
+import java.util.function.Function;
 
 public class blockStateProvider  extends BlockStateProvider {
     public blockStateProvider(DataGenerator gen, ExistingFileHelper exFileHelper) {
@@ -13,6 +22,17 @@ public class blockStateProvider  extends BlockStateProvider {
 
     @Override
     protected void registerStatesAndModels() {
+        BlockModelBuilder modelinjector = models().cube("injector",
+                modLoc("block/down"),
+                modLoc("block/top"),
+                modLoc("block/front"),
+                modLoc("block/back"),
+                modLoc("block/sides"),
+                modLoc("block/sides"));
+        //horizontalBlock(ModBlocks.INJECTOR.get(), modelinjector, 0);
         simpleBlock(ModBlocks.INJECTOR.get());
+
     }
+
+
 }
