51c2f0f63dfe1f4df044132e8af5d6ccefe994ff
criscky <cristianyoshida@ymail.com>
2021-11-24 12:04:33 -0300

    updating

--- src/main/java/com/criscky/armourbeyond/integration/jei/IntegrationJEI.java
+++ src/main/java/com/criscky/armourbeyond/integration/jei/IntegrationJEI.java
@@ -2,12 +2,15 @@ package com.criscky.armourbeyond.integration.jei;
 
 import com.criscky.armourbeyond.ArmourBeyond;
 import com.criscky.armourbeyond.crafting.recipes.InjectorRecipe;
+import com.criscky.armourbeyond.setup.ModBlocks;
 import com.criscky.armourbeyond.setup.ModRecipes;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.registration.IRecipeCatalystRegistration;
 import mezz.jei.api.registration.IRecipeCategoryRegistration;
 import mezz.jei.api.registration.IRecipeRegistration;
 import net.minecraft.client.Minecraft;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.RecipeManager;
 import net.minecraft.util.ResourceLocation;
 
@@ -31,4 +34,11 @@ public class IntegrationJEI implements IModPlugin {
         RecipeManager rm = Objects.requireNonNull(Minecraft.getInstance().level).getRecipeManager();
         registration.addRecipes(rm.getAllRecipesFor(ModRecipes.Types.INJECTION).stream().filter(r -> r instanceof InjectorRecipe).collect(Collectors.toList()), InjectorRecipeCategory.UID);
     }
+
+
+    @Override
+    public void registerRecipeCatalysts(IRecipeCatalystRegistration registration) {
+        registration.addRecipeCatalyst(new ItemStack(ModBlocks.INJECTOR.get()), InjectorRecipeCategory.UID);
+    }
+
 }
